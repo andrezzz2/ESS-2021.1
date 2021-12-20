6ef072fff40238fd2a74a3062983d4f9c6a8c1d3
André <alfr2@cin.ufpe.br>
2021-12-20 19:45:00 -0300

    teste de historico de pedidos

---
+++ test-acceptance/features/historico.feature
@@ -0,0 +1,6 @@
+Feature: Historico page
+
+    Scenario: Acessar historico de pedidos
+        Given Eu estou na página inicial
+        When Eu clico na aba de "Historico de pedidos"
+        Then O site me redireciona para a página "Historico de pedidos"
---
+++ test-acceptance/features/step_definitions/historico-steps.js
@@ -0,0 +1,27 @@
+require('chromedriver');
+const seleniumWebdriver = require('selenium-webdriver');
+const {By} = require('selenium-webdriver');
+let driver = new seleniumWebdriver.Builder()
+                  .forBrowser('chrome')
+                  .build();
+
+const { Given, When, Then } = require('cucumber');
+const {expect} = require('chai');
+                  
+
+Given('Eu estou na página inicial', {timeout: 60 *1000}, async () => {
+    await driver.manage().window().maximize();
+    await driver.get('http://localhost:3000');
+})
+When('Eu clico na aba de {string}', async (string) => {
+    const historico = await driver.findElement(By.id('historico'));
+    //const vegetable = driver.findElement(By.className('tomatoes'));
+    //driver.findElement(By.name('q')).sendKeys('webdriver', Key.ENTER);
+    await driver.actions().click(historico).perform();
+})
+Then('O site me redireciona para a página {string}', async (string) => {
+    const current = await driver.getCurrentUrl();
+    //assert.equal(current, "http://localhost:3000/historico");
+    expect(current).equal("http://localhost:3000/historico");
+    //current.should.equal("http://localhost:3000/historico");
+})
